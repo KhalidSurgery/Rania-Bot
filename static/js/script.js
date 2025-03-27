457760933a17f36cb955338a90abfa6209d70894
KhalidSurgery <khfrcs@gmail.com>
2025-03-27 03:05:57 +0300

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({ message: message })
+                body: JSON.stringify({ question: message })
             });
 
             const data = await response.json();
