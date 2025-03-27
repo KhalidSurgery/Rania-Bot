1147d1825b18792434f6a0c8c7b45e56ec19f350
KhalidSurgery <khfrcs@gmail.com>
2025-03-27 15:17:39 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+# إعدادات API
+API_PROVIDER = "deepseek"
+DEEPSEEK_API_KEY = "sk-49f40433cf074abd9ef4c323aea2a2af"  # استبدلها بمفتاحك الحقيقي
+
+# إعدادات العيادة
+CLINIC_NAME = "عيادة الدكتور خالد حسون الجراحية"
+EMERGENCY_PHONE = "07826762340"
+CLINIC_ADDRESS = "بغداد/ حي السلام (الطوبجي) / شارع السوق الشعبي"
+WORKING_HOURS = "4 عصراً حتى 9 مساءً (السبت-الخميس)"
+CONSULTATION_FEE = "20,000 دينار عراقي"
+
+# إعدادات الواتساب (اختياري)
+WHATSAPP_NUMBER = "9647706191881"
+CALLMEBOT_API_KEY = "6691057"  # استبدلها بمفتاحك إذا كنت تريد هذه الميزة
